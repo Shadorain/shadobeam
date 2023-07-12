e54153c2924ec6ef046bf2ce995e3faabf2b5e8b
Shadorain <jordanf7517@gmail.com>
2023-07-11 22:45:31 -0400

    wip(setup): Setup basic protobufs and demo implementation.

--- beacon/src/main.rs
+++ beacon/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use tasks::{greeter_server::GreeterServer, HelloReply, HelloRequest};
+use tonic::{transport::Server, Request, Response, Status};
+
+use crate::tasks::greeter_server::Greeter;
+
+pub mod tasks {
+    tonic::include_proto!("tasks");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        Ok(Response::new(tasks::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        }))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
--- implant/src/main.rs
+++ implant/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tasks::greeter_client::GreeterClient;
+use tasks::HelloRequest;
+
+pub mod tasks {
+    tonic::include_proto!("tasks");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
